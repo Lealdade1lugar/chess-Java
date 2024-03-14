0dec8afab1066d9a5dc202362df6721d6cda53af
Ilto Neto <iltoneto400@gmail.com>
2024-03-13 22:13:27 -0300

    Classe: Position

--- application/Program.java
+++ application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
         
-        System.out.println("Jogo de Xadrez");
+        Position p = new Position(1,2);
+        System.out.println(p);
 
     }
     
